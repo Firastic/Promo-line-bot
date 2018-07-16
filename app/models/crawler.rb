555c9d591a9a19138d6d5b44c881c2a73ec2d4e4
Adyaksa Wisanggeni <adyaksa.wisanggeni@midtrans.com>
2018-07-16 10:07:57 +0700

    Remove pry from program

--- app/models/crawler.rb
+++ app/models/crawler.rb
@@ -2,11 +2,9 @@ require 'open-uri'
 require 'promo.rb'
 class Crawler
   def crawl
-    binding.pry
     page =  Nokogiri::HTML(open("https://www.tokopedia.com/promo/"))
     title = page.css("div.promotion-description p")
     source = page.css("div.promotion-cta a")
-    #binding.pry
     title.each_with_index do |val,index|
       if(!Promo.where(title: parse_title(title[index].text),  source: "tokopedia", link: source[index]["href"]).exists?)
         Promo.create(title: parse_title(title[index].text), source: "tokopedia", link: source[index]["href"])
