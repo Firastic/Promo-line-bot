7f10b974554817c016dfd0d93db753ec8531e8c4
Adyaksa Wisanggeni <adyaksa.wisanggeni@midtrans.com>
2018-07-17 23:32:32 +0700

    Fix Environment variable

--- app/models/event.rb
+++ app/models/event.rb
@@ -57,8 +57,8 @@ class Event
 private
   def client
     @client ||= Line::Bot::Client.new { |config|
-    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    config.channel_secret = ENV["LINE_BOT_CHANNEL_SECRET"]
+    config.channel_token = ENV["LINE_BOT_CHANNEL_TOKEN"]
     }
   end
 end
